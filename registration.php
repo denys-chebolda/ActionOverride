6bdaa199ae3c52a069ae658270e3669ea7798de5
Denys Chebolda <denys.chebolda@scandiweb.com>
2022-12-13 16:30:03 +0200

    First try

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @category    Scandiweb
+ * @package     MenuOrganizer
+ * @author      Denys Chebolda <info@scandiweb.com>
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Scandiweb_MenuOrganizer',
+    __DIR__
+);
